c0ab1daded1f89bc7b759c26f3510bbe04c56d2e
kumarjee alluri <kalluri@cisco.com>
2015-02-28 22:24:24 +0530

    Adding pom generator code

--- AntToMavenTool/src/com/infy/hackathon/na/utility/PomDependencyGenerator.java
+++ AntToMavenTool/src/com/infy/hackathon/na/utility/PomDependencyGenerator.java
@@ -33,7 +33,7 @@ public class PomDependencyGenerator {
 			ResourceBundle bundle = ResourceBundle.getBundle("MavenResources");
 			System.out.println("Begining to unzip the uploaded file....");
 			FileUtility.extractUploadedZipProject(bundle.getString(AntToMavenConstants.UPLOADED_ZIP_DIR), bundle.getString(AntToMavenConstants.UNZIPPED_PROJECT_DIR));
-			
+			PomDependencyGenerator.getPomDependencies();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -43,15 +43,19 @@ public class PomDependencyGenerator {
 		
 	}
 	
-	public static void getArtifactId(String searchkey, String jarFilePath) throws NoSuchAlgorithmException, IOException{
+	public static MavenArtifactBean getArtifactId(String jarFilePath) throws NoSuchAlgorithmException, IOException{
+		
 		String jarName = jarFilePath.substring(jarFilePath.lastIndexOf("\\")+1,jarFilePath.indexOf("jar")-1);
-		String version = jarName.substring(jarName.lastIndexOf("-")+1);
+		
+		String version = Character.isDigit(jarName.substring(jarName.lastIndexOf("-")+1).charAt(0))?jarName.substring(jarName.lastIndexOf("-")+1):"";
+		String splitNames[]=jarName.split("-");
+		jarName =  Character.isDigit(splitNames[splitNames.length-1].charAt(0))? jarName.substring(0, jarName.lastIndexOf("-")):jarName;
 		String checkSum = ChecksumUtility.getCheckSum(jarFilePath);
+		System.out.println("JarName::"+jarName);
 		JsonObject obj;
 		obj = PomDependencyGenerator.invokeMavenRepoService(CHECKSUM_SEARCH, checkSum);
-		//
 		List<MavenArtifactBean> mavenArtifacts = new ArrayList<MavenArtifactBean>();
-		mavenArtifacts.addAll(PomDependencyGenerator.getAllMavenArtifactBeans(obj, searchkey));
+		mavenArtifacts.addAll(PomDependencyGenerator.getAllMavenArtifactBeans(obj, jarName));
 		MavenArtifactBean returnMavenArtifactBean = null;
 		if (mavenArtifacts != null) {
 			Collections.sort(mavenArtifacts,
@@ -60,8 +64,8 @@ public class PomDependencyGenerator {
 					.size() == 0) ? null : mavenArtifacts.get(0);
 		}
 		if (mavenArtifacts == null || mavenArtifacts.size() == 0) {
-			obj = PomDependencyGenerator.invokeMavenRepoService(JARNAME_SEARCH, searchkey);
-			mavenArtifacts.addAll(getAllMavenArtifactBeans(obj, searchkey));
+			obj = PomDependencyGenerator.invokeMavenRepoService(JARNAME_SEARCH, jarName);
+			mavenArtifacts.addAll(getAllMavenArtifactBeans(obj, jarName));
 			Collections.sort(mavenArtifacts,
 					new MavenArtifactBean().new SortBasedOnRank());
 			returnMavenArtifactBean = (mavenArtifacts == null || mavenArtifacts
@@ -71,8 +75,9 @@ public class PomDependencyGenerator {
 
 		}
 		if (mavenArtifacts == null || mavenArtifacts.size() == 0) {
-			String maventempDir = AntToMavenConstants.JAR_PROCESSING_DIR + "lib";
-			File file = new File(maventempDir);
+			ResourceBundle bundle = ResourceBundle.getBundle("MavenResources");
+			String targetFolder = bundle.getString("APP_NAME_SPACE")+bundle.getString("SRC_MAIN_WEBINF_LIB")+"\\";
+			File file = new File(targetFolder);
 			if (!file.exists()) {
 				FileUtils.forceMkdir(file);
 			}
@@ -90,9 +95,10 @@ public class PomDependencyGenerator {
 			returnMavenArtifactBean.setG(jarName);
 			returnMavenArtifactBean.setLatestVersion(version);
 			returnMavenArtifactBean.setScope("system");
-			returnMavenArtifactBean.setSystemPath(maventempDir
+			returnMavenArtifactBean.setSystemPath(targetFolder
 					+ jarFilePath.substring(jarFilePath.lastIndexOf("\\") + 1));
 		}
+		return returnMavenArtifactBean;
 		
 	}
 	
@@ -157,23 +163,41 @@ public class PomDependencyGenerator {
 	}
 	
 	
-	public void getPomDependencies(){
+	public static List<MavenArtifactBean>  getPomDependencies(){
 		System.out.println("Started bui");
 		ResourceBundle bundle = ResourceBundle.getBundle("MavenResources");
 		File jarHeadDir = new File(bundle.getString(AntToMavenConstants.LIB_DIR));
 		File[] jarDir = jarHeadDir.listFiles();
-
-		for(File jarFile : jarDir){
-			String jarFilePath = jarFile.toString();
-			String jarName = jarFilePath.substring(jarFilePath.lastIndexOf("\\")+1,jarFilePath.indexOf("jar")-1);
-			String version = jarName.substring(jarName.lastIndexOf("-")+1);
-			jarName = jarName.substring(0, jarName.lastIndexOf("-"));
-			if(jarFilePath.endsWith(".jar")){
-				
-			}else
-				continue;
-			
+		List<MavenArtifactBean> dependencyBeans = null; 
+		if (jarDir != null) {
+			dependencyBeans = new ArrayList<MavenArtifactBean>();
+			for (File jarFile : jarDir) {
+				String jarFilePath = jarFile.toString();
+				/*
+				 * String jarName =
+				 * jarFilePath.substring(jarFilePath.lastIndexOf
+				 * ("\\")+1,jarFilePath.indexOf("jar")-1); String version =
+				 * jarName.substring(jarName.lastIndexOf("-")+1); jarName =
+				 * jarName.substring(0, jarName.lastIndexOf("-"));
+				 */
+				if (jarFilePath.endsWith(".jar")) {
+					try {
+						MavenArtifactBean artifactId = PomDependencyGenerator
+								.getArtifactId(jarFilePath);
+						if (artifactId != null)
+							dependencyBeans.add(artifactId);
+					} catch (NoSuchAlgorithmException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} else
+					continue;
+			}
 		}
+		return dependencyBeans;
 		
 	}
 
