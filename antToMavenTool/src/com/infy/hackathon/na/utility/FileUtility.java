dd857305d6514e2606fced64f71a8fdfbc8cc5e0
kumarjee alluri <kalluri@cisco.com>
2015-02-28 13:07:34 +0530

    Added jars

--- antToMavenTool/src/com/infy/hackathon/na/utility/FileUtility.java
+++ antToMavenTool/src/com/infy/hackathon/na/utility/FileUtility.java
@@ -1,5 +1,11 @@
 package com.infy.hackathon.na.utility;
 
+import java.io.File;
+
 public class FileUtility {
+	
+	public static void extractUploadedZipProject(String sourceDir, String targetDir){
+		File file = new File(sourceDir);
+	}
 
 }
